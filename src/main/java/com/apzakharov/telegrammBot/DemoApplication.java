ba2cc1e445e7d6867891f90edfda4ba7aad49cd3
Sagat9881 <Sagat9881@gmail.com>
2021-09-23 13:01:52 +0300

    0.0.1 initial commit

--- src/main/java/com/apzakharov/telegrammBot/DemoApplication.java
+++ src/main/java/com/apzakharov/telegrammBot/DemoApplication.java
@@ -1,11 +1,13 @@
 package com.apzakharov.telegrammBot;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
+@EnableProcessApplication
 @EnableScheduling
 public class DemoApplication {
     public static void main(String[] args) {
