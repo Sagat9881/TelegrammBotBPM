f20b6f9c55b5396fa68c9b2f7a2ed0d7d5aad769
Sagat9881 <Sagat9881@gmail.com>
2021-09-26 17:40:32 +0300

    0.0.4

--- src/main/java/com/apzakharov/telegrammBot/bpmn/bpmProcessImpl/botIncomingMessageProcess/ProcessCommand.java
+++ src/main/java/com/apzakharov/telegrammBot/bpmn/bpmProcessImpl/botIncomingMessageProcess/ProcessCommand.java
@@ -1,15 +1,20 @@
 package com.apzakharov.telegrammBot.bpmn.bpmProcessImpl.botIncomingMessageProcess;
 
 import com.apzakharov.telegrammBot.bot.BotContext;
+import com.apzakharov.telegrammBot.bot.ChatBot;
 import com.apzakharov.telegrammBot.bpmn.service.CamundaProcessService;
 import com.apzakharov.telegrammBot.model.User;
 import com.apzakharov.telegrammBot.service.UserService;
+import lombok.extern.log4j.Log4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProcessCommand implements JavaDelegate {
+    private static final Logger LOGGER = LogManager.getLogger(ProcessCommand.class);
     CamundaProcessService camundaProcessService;
     UserService userService;
 
@@ -17,6 +22,7 @@ public class ProcessCommand implements JavaDelegate {
     public void execute(DelegateExecution delegateExecution) throws Exception {
 //        Long chatID = camundaProcessService.getChatID(delegateExecution);
         Long chatID = (Long) delegateExecution.getVariable("ChatID");
+        LOGGER.info("ProcessCommand for chatID: "+ chatID);
 
         String outputText = "Тестовая заглушка работы сценария ответа на работу команды";
         camundaProcessService.sendMessage(outputText,chatID);
--- src/main/java/com/apzakharov/telegrammBot/bpmn/service/UserProcessServiceImpl.java
+++ src/main/java/com/apzakharov/telegrammBot/bpmn/service/UserProcessServiceImpl.java
@@ -2,11 +2,14 @@ package com.apzakharov.telegrammBot.bpmn.service;
 
 import com.apzakharov.telegrammBot.bot.BotContext;
 import com.apzakharov.telegrammBot.bot.BotStateBPMN;
+import com.apzakharov.telegrammBot.bpmn.bpmProcessImpl.botIncomingMessageProcess.ProcessCommand;
 import com.apzakharov.telegrammBot.bpmn.dto.ProcessStartRequestBody;
 import com.apzakharov.telegrammBot.bpmn.dto.ProcessVariable;
 import com.apzakharov.telegrammBot.model.User;
 import com.apzakharov.telegrammBot.repo.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Nonnull;
@@ -21,16 +24,22 @@ public class UserProcessServiceImpl implements UserProcessService {
 
     private UserRepository userRepository;
     static final String JSON_TYPE_STRING = "String";
-    static final String ProcessURL = "https://telegramm-bot-bpm.herokuapp.com/engine-rest/process-definition/key/process-incoming-message/start";
+    static final String ProcessURL = "http://telegramm-bot-bpm.herokuapp.com/engine-rest/process-definition/key/process-incoming-message/start";
     ObjectMapper objectMapper;
     CamundaProcessService camundaProcessService;
+
+    private static final Logger LOGGER = LogManager.getLogger(ProcessCommand.class);
 
     @Override
     public void processStart(@Nonnull BotContext contex) throws Exception {
 
+        LOGGER.info("Start New UserProcessAnswer for contex: \n"+ objectMapper.writeValueAsString(contex));
+
         User user = contex.getUser();
         Long chatID = user.getChatId();
         String input = contex.getInput();
+
+        LOGGER.info("Context for process: \n"+ objectMapper.writeValueAsString(contex));
 //        Integer stateID = user.getStateId();
 
 //        BotStateBPMN usersBotState = BotStateBPMN.getByBotStateBPMNID(stateID)
@@ -50,8 +59,12 @@ public class UserProcessServiceImpl implements UserProcessService {
             variables.put("ChatID", chatID);
             variables.put("Input", input);
 
+            LOGGER.info("Variabls for process: "+ objectMapper.writeValueAsString(variables));
+
             ProcessStartRequestBody processBody = new ProcessStartRequestBody();
             processBody.setVariables(variables);
+
+            LOGGER.info("StartProcess for Url: \n"+ ProcessURL+"\n"+" and body: \n"+objectMapper.writeValueAsString(processBody));
 
             camundaProcessService.processStart(ProcessURL, processBody);
 
