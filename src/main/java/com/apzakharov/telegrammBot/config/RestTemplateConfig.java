9c9da4f329d08c80c37c76a9d51dd93cdef2af53
Sagat9881 <Sagat9881@gmail.com>
2021-09-26 18:49:17 +0300

    0.0.4

--- src/main/java/com/apzakharov/telegrammBot/config/RestTemplateConfig.java
+++ src/main/java/com/apzakharov/telegrammBot/config/RestTemplateConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfig {
+public class RestTemplateConfig extends RestTemplate{
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
