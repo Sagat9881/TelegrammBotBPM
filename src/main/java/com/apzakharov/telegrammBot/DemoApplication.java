3ca57fac9676067af014b6adde09f0b0bde46fd9
Sagat9881 <Sagat9881@gmail.com>
2021-11-16 21:53:32 +0300

    0.1.4 - Добавление чернового логгирования, замена конфигурации обратно к H2 в оперативной памяти, удаление стек трейсов при ошибках

--- src/main/java/com/apzakharov/telegrammBot/DemoApplication.java
+++ src/main/java/com/apzakharov/telegrammBot/DemoApplication.java
@@ -4,10 +4,12 @@ import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
+@EnableJpaRepositories
 @EnableProcessApplication
 //@EnableScheduling
 public class DemoApplication {
