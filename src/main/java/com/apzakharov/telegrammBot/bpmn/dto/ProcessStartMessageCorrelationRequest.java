66da12093d742b1364eb832de35deb9c738d063e
Sagat9881 <Sagat9881@gmail.com>
2021-11-22 22:37:58 +0300

    0.2.1 - oh shit, here we go again

--- src/main/java/com/apzakharov/telegrammBot/bpmn/dto/ProcessStartMessageCorrelationRequest.java
+++ src/main/java/com/apzakharov/telegrammBot/bpmn/dto/ProcessStartMessageCorrelationRequest.java
@@ -1,14 +1,16 @@
 package com.apzakharov.telegrammBot.bpmn.dto;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Data
 @Builder
+@ToString
 @Component
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProcessStartMessageCorrelationRequest {
 
     private String messageName;
